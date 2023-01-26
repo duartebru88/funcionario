01157ba00144065f0ae61f4aaf20471bc174c0c2
carlassilva <carlas_fs@hotmail.com>
2023-01-26 13:13:28 -0300

    Versão Carlas - Não grava funcionario

--- src/main/java/br/com/funcionario/ada/controller/FuncionarioController.java
+++ src/main/java/br/com/funcionario/ada/controller/FuncionarioController.java
@@ -1,11 +1,99 @@
 package br.com.funcionario.ada.controller;
 
+import br.com.funcionario.ada.dto.*;
+import br.com.funcionario.ada.entity.Funcionario;
+import br.com.funcionario.ada.service.impl.FuncionarioServiceImpl;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioServiceImpl funcionarioService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    public ResponseEntity<FuncionarioSaveResponseDto> save(@RequestBody @Valid FuncionarioSaveRequestDto funcionarioRequest){
+
+        log.info(funcionarioRequest.toString());
+        Funcionario funcionario = modelMapper.map(funcionarioRequest, Funcionario.class);
+
+        funcionarioService.save(funcionario);
+        FuncionarioSaveResponseDto funcionarioResponse = new FuncionarioSaveResponseDto();
+        funcionarioResponse.setId(funcionario.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(funcionarioResponse);
+
+    }
+    @GetMapping(value = "/{idFuncionario}")
+    public ResponseEntity<FuncionarioResponseDto> getById(@PathVariable(value = "idFuncionario") Long id) {
+
+        Optional<Funcionario> funcionario = funcionarioService.findById(id);
+
+        if (funcionario.isPresent()) {
+            FuncionarioResponseDto funcionarioResponse = modelMapper.map(funcionario.get(), FuncionarioResponseDto.class);
+            return ResponseEntity.ok(funcionarioResponse);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PatchMapping("/{idFuncionario}/{bonusSalarial}")
+    public ResponseEntity updatebonusSalarial(@PathVariable Long idFuncionario, @PathVariable BigDecimal bonusSalarial) {
+        //update
+        log.info("idFuncionario: {} bonusSalarial: {}", idFuncionario, bonusSalarial);
+        Optional<Funcionario> funcionario = funcionarioService.findById(idFuncionario);
+
+        if (funcionario.isPresent()) {
+            funcionario.get().setBonusSalarial(bonusSalarial);
+            funcionarioService.save(funcionario.get());
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    @DeleteMapping("/{idFuncionario}")
+    public ResponseEntity<String> delete(@PathVariable Long idFuncionario) {
+
+        Optional<Funcionario> funcionario = funcionarioService.findById(idFuncionario);
+        if (funcionario.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        funcionarioService.deleteById(idFuncionario);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{idFuncionario}")
+    public ResponseEntity update(@PathVariable Long idFuncionario, @RequestBody @Valid FuncionarioSaveRequestDto funcionarioRequest) {
+
+        Optional<Funcionario> funcionarioOptional = funcionarioService.findById(idFuncionario);
+
+        if (funcionarioOptional.isPresent()) {
+            Funcionario funcionario = modelMapper.map(funcionarioRequest, Funcionario.class);
+            funcionarioService.save(funcionario);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Funcionario>> findAll() {return ResponseEntity.ok(funcionarioService.findAll());
+    }
+
 }
--- src/main/java/br/com/funcionario/ada/dto/FuncionarioResponseDto.java
+++ src/main/java/br/com/funcionario/ada/dto/FuncionarioResponseDto.java
@@ -3,13 +3,16 @@ package br.com.funcionario.ada.dto;
 import br.com.funcionario.ada.entity.Cargo;
 import br.com.funcionario.ada.entity.Endereco;
 import br.com.funcionario.ada.entity.enums.PerfilEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Getter
 @Setter
 public class FuncionarioResponseDto {
-
         private Long id;
         private String nome;
         private String cpf;
